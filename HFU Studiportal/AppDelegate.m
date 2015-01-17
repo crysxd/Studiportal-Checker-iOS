cac2edd5237f8779ac70e17fe0209bd7321022a5
crysxd <c.wuerthner@me.com>
2015-01-17 14:05:55 +0100

    Requesting permission to show local notifications
    Also show a dialog if a local notification is raised while the app is
    in foreground

--- HFU Studiportal/AppDelegate.m
+++ HFU Studiportal/AppDelegate.m
@@ -17,7 +17,12 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    if ([UIApplication instancesRespondToSelector:@selector(registerUserNotificationSettings:)]){
+        [application registerUserNotificationSettings:[UIUserNotificationSettings settingsForTypes:UIUserNotificationTypeAlert|UIUserNotificationTypeBadge|UIUserNotificationTypeSound categories:nil]];
+    }
+
     return YES;
+    
 }
 
 - (void)applicationWillResignActive:(UIApplication *)application {
@@ -40,6 +45,16 @@
 
 - (void)applicationWillTerminate:(UIApplication *)application {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+}
+
+- (void) application:(UIApplication *)application didReceiveLocalNotification:    (UILocalNotification *)notification {
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:[notification.userInfo objectForKey: @"title"]
+                                                    message:[notification.userInfo objectForKey: @"text"]
+                                                   delegate:nil
+                                          cancelButtonTitle:@"OK"
+                                          otherButtonTitles:nil];
+    [alert show];
+
 }
 
 @end
