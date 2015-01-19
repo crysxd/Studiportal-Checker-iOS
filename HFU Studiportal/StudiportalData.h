2970266589f64f38aec880f5b885e778a1059ccd
crysxd <c.wuerthner@me.com>
2015-01-19 18:05:21 +0100

    Added StudiportalData class
    Saving and loading to or from disk is not yet implemented

---
+++ HFU Studiportal/StudiportalData.h
@@ -0,0 +1,33 @@
+//
+//  StudiportalData.h
+//  HFU Studiportal
+//
+//  Created by Christian Würthner on 18.01.15.
+//  Copyright (c) 2015 Hochschule Furtwangen University. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "ExamCategory.h"
+#import "TFHpple.h"
+#import "Seperator.h"
+
+@interface StudiportalData : NSObject
+
+@property NSMutableArray *categoryList;
+
+-(id) initWithHtmlTable:(NSString*)html;
+-(id) initFromDisk;
+-(NSArray*) findChangedExams:(StudiportalData*)otherInstance;
+-(NSArray*) searchExams:(NSString*)query;
+-(void) save;
+-(NSMutableArray*) parseTable:(NSString*)html;
+-(Exam*) createExam:(TFHppleElement*)element;
+-(ExamCategory*) createExamCategory:(TFHppleElement*)element currentExamCategory:(ExamCategory*)current;
+-(NSArray*) allExams;
+-(void) addCategory:(ExamCategory*)category;
+-(void) removeCategory:(ExamCategory*)category;
+-(void) removeCategoryWithIndex:(NSUInteger)index;
+-(NSUInteger) categoryCount;
+-(ExamCategory*) getCategory:(NSUInteger)index;
+
+@end
---
+++ HFU Studiportal/StudiportalData.m
@@ -0,0 +1,163 @@
+//
+//  StudiportalData.m
+//  HFU Studiportal
+//
+//  Created by Christian Würthner on 18.01.15.
+//  Copyright (c) 2015 Hochschule Furtwangen University. All rights reserved.
+//
+
+#import "StudiportalData.h"
+
+@implementation StudiportalData
+@synthesize categoryList = _categoryList;
+
+-(id) initWithHtmlTable:(NSString*)html {
+    self = [self init];
+    _categoryList = [self parseTable:html];
+    return self;
+    
+}
+
+-(id) initFromDisk {
+    _categoryList = [[NSMutableArray alloc] init];
+    return [self init];
+    
+}
+
+-(NSArray*) findChangedExams:(StudiportalData*)otherInstance {
+    return [[NSArray alloc] init];
+    
+}
+
+-(NSArray*) searchExams:(NSString*)query {
+    return [[NSArray alloc] init];
+
+}
+
+-(void) save {
+    
+}
+
+-(NSMutableArray*) parseTable:(NSString*)html {
+    NSMutableArray* categoryList = [[NSMutableArray alloc] init];
+    NSData *data = [html dataUsingEncoding:NSUTF8StringEncoding];
+    TFHpple *parser = [TFHpple hppleWithHTMLData:data encoding:@"UTF-8"];
+
+    NSArray *rows = [parser searchWithXPathQuery:@"//tr"];
+    ExamCategory *current = [[ExamCategory alloc] initWithCategoryName:@"Unmatched"];
+    
+    
+    for (TFHppleElement* row in rows) {
+        NSArray *ths = [row searchWithXPathQuery:@"//th"];
+        
+        if(ths.count > 0) {
+            if(ths.count > 1) {
+                continue;
+                
+            }
+            
+            ExamCategory* newCategory = [self createExamCategory:row currentExamCategory:current];
+            if(newCategory != nil) {
+                current = newCategory;
+                [categoryList addObject:current];
+                
+            }
+        } else {
+            [current addExam:[self createExam:row]];
+            
+        }
+    }
+    
+    return categoryList;
+    
+}
+
+-(Exam*) createExam:(TFHppleElement*)element {
+    NSArray *cols = [element searchWithXPathQuery:@"//td"];
+    
+    Exam *e = [[Exam alloc] initWithExamNo:[[cols objectAtIndex:0] text]];
+    
+    NSUInteger offset = 0;
+    
+    for (NSUInteger i=0; i<cols.count; i++) {
+        TFHppleElement *col = [cols objectAtIndex:i];
+        NSString *colspan = [[col attributes] objectForKey:@"colspan"];
+        
+        if(colspan != nil && colspan.length > 0) {
+            offset += [colspan integerValue];
+            
+        }
+        
+        NSString *text = [col.text stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceCharacterSet]];
+        
+        switch(i + offset) {
+            case 1: e.name = text; break;
+            case 2: e.bonus = text; break;
+            case 3: e.malus = text; break;
+            case 4: e.ects = text; break;
+            case 5: e.sws = text; break;
+            case 6: e.semester = text; break;
+            case 7: e.kind = text; break;
+            case 8: e.tryCount = text; break;
+            case 9: e.grade = text; break;
+            case 10: e.state = text; break;
+            case 11: e.comment = text; break;
+            case 12: e.resignation = text; break;
+            case 13: e.note = text; break;
+                
+        }
+    }
+    
+    return e;
+    
+}
+
+-(ExamCategory*) createExamCategory:(TFHppleElement*)element currentExamCategory:(ExamCategory*)current {
+    NSString *name = [element content];
+    
+    if(name.length == 0) {
+        [current addExam:[[Seperator alloc] init]];
+        return nil;
+        
+    }
+    
+    return [[ExamCategory alloc] initWithCategoryName:name];
+    
+}
+
+-(NSArray*) allExams {
+    NSMutableArray* exams = [[NSMutableArray alloc] init];
+    
+    for (ExamCategory* c in self.categoryList) {
+        [exams addObjectsFromArray:c.examList];
+        
+    }
+    
+    return exams;
+
+}
+
+-(void) addCategory:(ExamCategory*)category {
+    [self.categoryList addObject:category];
+    
+}
+
+-(void) removeCategory:(ExamCategory*)category {
+    [self.categoryList removeObject:category];
+}
+
+-(void) removeCategoryWithIndex:(NSUInteger)index {
+    [self.categoryList removeObjectAtIndex:index];
+}
+
+-(NSUInteger) categoryCount {
+    return self.categoryList.count;
+    
+}
+
+-(ExamCategory*) getCategory:(NSUInteger)index {
+    return [self.categoryList objectAtIndex:index];
+    
+}
+
+@end
