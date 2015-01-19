3330484ef4d545e5350a1b998d3e34ecec675a5f
crysxd <c.wuerthner@me.com>
2015-01-19 18:01:04 +0100

    Added Separator implementation

---
+++ HFU Studiportal/Seperator.m
@@ -0,0 +1,13 @@
+//
+//  Seperator.m
+//  HFU Studiportal
+//
+//  Created by Christian Würthner on 18.01.15.
+//  Copyright (c) 2015 Hochschule Furtwangen University. All rights reserved.
+//
+
+#import "Seperator.h"
+
+@implementation Seperator
+
+@end
