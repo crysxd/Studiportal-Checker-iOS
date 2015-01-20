e19b74d54a3177dfa290d570b779fda84c72dedf
crysxd <c.wuerthner@me.com>
2015-01-20 16:58:31 +0100

    Added on side menu controller

---
+++ HFU Studiportal/CategoryMenuController.h
@@ -0,0 +1,13 @@
+//
+//  SideMenuController.h
+//  HFU Studiportal
+//
+//  Created by Christian Würthner on 19.01.15.
+//  Copyright (c) 2015 Hochschule Furtwangen University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CategoryMenuController : UIViewController
+
+@end
---
+++ HFU Studiportal/CategoryMenuController.m
@@ -0,0 +1,18 @@
+//
+//  SideMenuController.m
+//  HFU Studiportal
+//
+//  Created by Christian Würthner on 19.01.15.
+//  Copyright (c) 2015 Hochschule Furtwangen University. All rights reserved.
+//
+
+#import  "CategoryMenuController.h"
+
+@implementation CategoryMenuController
+
+-(UIStatusBarStyle)preferredStatusBarStyle{
+    return UIStatusBarStyleLightContent;
+    
+}
+
+@end
